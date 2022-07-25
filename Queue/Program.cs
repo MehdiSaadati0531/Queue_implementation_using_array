1f4355fcfda2a109fd5625f809174e8ae03eb1da
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-07-25 01:33:49 -0700

    Add files via upload

---
+++ Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Queue;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Queue_Array a = new Queue_Array(3);
+
+
+
+a.En(4);
+a.En(45);
+a.En(7);
+a.Queue_display();
+a.En(8);
+a.De();
+a.De();
+a.De();
+a.En(5);
+a.Queue_display();
---
+++ Queue/Queue_Array.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class Queue_Array
+    {
+        private int[] Queue;
+        private int rear;
+        private int front;
+
+        public Queue_Array(int SizeQueue)
+        {
+            Queue = new int[SizeQueue];
+            front = 0;
+            rear = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            if (rear <= front)
+                return true;
+            return false;
+        }
+
+        public bool IsFull()
+        {
+            if (Queue.Length <= rear)
+                return true;
+            return false;
+        }
+
+        public void De()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue UnderFlow");
+            }
+            else
+            {
+                rear--;
+                for (int i = 0; i < Queue.Length - 1; i++)
+                {
+                    Queue[i] = Queue[i + 1];
+                }
+            }
+        }
+
+        public void En(int newnumber)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue OverFlow");
+            }
+            else
+            {
+                Queue[rear] = newnumber;
+                rear++;
+            }
+        }
+
+        public void Queue_display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue UnderFlow");
+            }
+            else
+            {
+                for (int i = front; i < rear; i++)
+                {
+                    Console.WriteLine(Queue[i]);
+                }
+            }
+        }
+    }
+}
